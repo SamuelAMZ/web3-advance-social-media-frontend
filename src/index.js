ad3b2d8518a74d77a704d82ca0f8d1bc61198dde
sam_dev <samueldevpro09@gmail.com>
2022-09-06 08:12:07 +0200

    css compressed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "../src/styles/style.css";
+import "../src/styles/style.min.css";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
