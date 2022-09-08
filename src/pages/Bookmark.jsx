6db295cf993f5e994f0fdf716d88cd938e3f78c5
sam_dev <samueldevpro09@gmail.com>
2022-09-08 12:29:20 +0200

    bookmark page added

--- src/pages/Bookmark.jsx
+++ src/pages/Bookmark.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import Appbar from "../components/Appbar";
 import Third from "../components/Third";
+import Post from "../components/Post";
 
 const Explore = () => {
   return (
     <>
-      <div className="home">
+      <div className="bookmark">
         <Appbar />
-        <div className="actual-home">Bookmark</div>
+        <div className="actual-bookmark">
+          <div className="books">
+            <h3 className="date">12-12-2022</h3>
+            <div className="book">
+              <Post />
+              <Post />
+            </div>
+          </div>
+          <div className="books">
+            <h3 className="date">12-13-2022</h3>
+            <div className="book">
+              <Post />
+            </div>
+          </div>
+          <div className="books">
+            <h3 className="date">12-12-2022</h3>
+            <div className="book">
+              <Post />
+              <Post />
+              <Post />
+              <Post />
+            </div>
+          </div>
+        </div>
         <Third />
       </div>
     </>
